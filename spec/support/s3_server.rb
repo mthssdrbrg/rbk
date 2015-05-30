e94e1db8d8b224a309c009cde14ca22f9e561d38
Mathias Söderberg <mathias.soederberg@gmail.com>
2015-05-31 01:49:49 +0200

    Monkey patch FakeS3 to suppress logging
    The capturing logic didn't really work so well, for whatever reason,
    probably something to do with threads.

--- spec/support/s3_server.rb
+++ spec/support/s3_server.rb
@@ -2,6 +2,33 @@
 
 require 'fakes3'
 
+module FakeS3
+  class FileStore
+    def puts(*args)
+    end
+  end
+
+  class XmlAdapter
+    def self.append_objects_to_list_bucket_result(lbr, objects)
+      return if objects.nil?
+
+      objects.each do |s3_object|
+        lbr.Contents do |contents|
+          contents.Key(s3_object.name)
+          contents.LastModified(s3_object.modified_date)
+          contents.ETag("\"#{s3_object.md5}\"")
+          contents.Size(s3_object.size)
+          contents.StorageClass("STANDARD")
+
+          contents.Owner do |owner|
+            owner.ID("abc")
+            owner.DisplayName("You")
+          end
+        end
+      end
+    end
+  end
+end
 
 module Support
   class S3Server
@@ -18,10 +45,8 @@ module Support
 
     def start
       @thread = Thread.new do
-        capture do
-          @server.mount '/', FakeS3::Servlet, @store, 'localhost'
-          @server.start
-        end
+        @server.mount '/', FakeS3::Servlet, @store, 'localhost'
+        @server.start
       end
     end
 
@@ -29,22 +54,5 @@ module Support
       @server.shutdown
       @thread.join
     end
-
-    private
-
-    def capture(stream=:stdout, &block)
-      s = nil
-      begin
-        stderr, stdout, $stderr, $stdout = $stderr, $stdout, StringIO.new, StringIO.new
-        result = (stream == :stdout) ? $stdout : $stderr
-        yield
-        s = result.string
-      ensure
-        $stderr, $stdout = stderr, stdout
-      end
-      puts s if ENV['SILENCE_LOGGING'] == 'no' && !s.empty?
-      s
-    end
-    alias_method :silence, :capture
   end
 end
